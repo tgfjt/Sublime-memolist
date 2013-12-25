19df616f34d72dc64a7bdfc62e178bc26c62b786
tgfjt <fujita@tagpanda.co.jp>
2013-12-25 18:43:30 +0900

    open show search for OSX

--- memolist.py
+++ memolist.py
@@ -1,8 +1,20 @@
-import os
-import datetime
-import subprocess
 import sublime
 import sublime_plugin
+import os, datetime, subprocess
+
+insert_text = [
+    "title: ",
+    "==========",
+    "date: ",
+    "tags: []",
+    "categories: []",
+    "- - -"
+]
+
+class MemolistInsertCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        global insert_text
+        self.view.insert(edit, 0, '\n'.join(insert_text))
 
 class MemolistOpenCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -15,9 +27,19 @@ class MemolistOpenCommand(sublime_plugin.WindowCommand):
     def settings(self):
         return sublime.load_settings("memolist.sublime-settings")
 
-    def open_memo_file(self, filename):
+    def open_memo_file(self, file_title):
+        global insert_text
+
+        if file_title == '':
+            return None
+
         dir_path = self.prepare_memo_dir()
-        self.window.open_file(dir_path + "/" + filename + '.md')
+        insert_text[0] += file_title
+        file_name = datetime.datetime.today().strftime("%Y-%m-%d-") + file_title
+
+        self.window.open_file(dir_path + "/" + file_name + '.md')
+        sublime.set_timeout(lambda:
+                self.window.run_command("memolist_insert"), 0)
 
     def prepare_memo_dir(self):
         memo_dir = os.path.expandvars(self.settings().get('memo_dir'))
@@ -31,15 +53,24 @@ class MemolistOpenCommand(sublime_plugin.WindowCommand):
             os.mkdir(path)
 
     def default_filename(self):
-        return datetime.datetime.today().strftime("%Y-%m-%d-")
+        global data
+        data[2] += datetime.datetime.today().strftime("%Y-%m-%d %H:%M")
+        return ''
 
 class MemolistShowCommand(sublime_plugin.WindowCommand):
     def run(self):
-        memo_dir = os.path.expandvars(self.settings().get('memo_dir'))
-        files = os.listdir(memo_dir)
+        self.memo_dir = os.path.expandvars(self.settings().get('memo_dir'))
+        self.files = os.listdir(self.memo_dir)
+
+        self.files.remove('.DS_Store')
  
-        for file in files:
-            print(file)
+        self.window.show_quick_panel(self.files, self.on_chosen)
+
+    def on_chosen(self, index):
+        if index == -1:
+          return
+
+        self.window.open_file(self.memo_dir + '/' + self.files[index])
 
     def settings(self):
         return sublime.load_settings("memolist.sublime-settings")
@@ -65,6 +96,7 @@ class MemolistSearchCommand(sublime_plugin.WindowCommand):
             except:
                 pass
 
+        self.results.remove('.DS_Store')
         self.window.show_quick_panel(self.results, self.on_chosen)
 
     def settings(self):
@@ -75,3 +107,4 @@ class MemolistSearchCommand(sublime_plugin.WindowCommand):
           return
 
         self.window.open_file(self.memo_dir + '/' + self.results[index])
+
